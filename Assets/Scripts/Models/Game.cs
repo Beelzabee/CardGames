69372d20d1b203134700d5084a8fff10b1e6c00a
Beelzabee <beelzabee@gmail.com>
2018-06-29 12:11:39 -0400

    First
    This project will be the first of my training in how to actually BUILD
    games. Instead of starting where I want to be I need to start where I
    should start.

---
+++ Assets/Scripts/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game {
+
+	//There should be several different types of card games
+	List <GameTypes> gt = new List<GameTypes> ();
+
+	public GameTypes[] ReturnGameTypes (){
+		GameTypes[] solution = gt.ToArray ();
+		return solution;
+	}
+
+}
